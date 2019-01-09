dc3f2c99a6446989bb2a930f93b3685481a4168a
kriskamumu <malenkauuuu@yandex.ru>
2019-01-09 11:16:47 +0300

    Добавлен поиск и красивый вывод

--- Кулинарная книга/DishBook/forms.py
+++ Кулинарная книга/DishBook/forms.py
@@ -1,10 +1,11 @@
 from app_config import mydb
 from models import Dish, Ingredient, Unit, Dish_Ingredient
-from wtforms import validators, Form, SubmitField, IntegerField, StringField
+from wtforms import validators, Form, SubmitField, IntegerField, StringField, FieldList
 from wtforms_alchemy import ModelForm
 from wtforms_alchemy.fields import QuerySelectField
 from wtforms_alchemy.validators import Unique
 from wtforms_components.fields import TimeField
+
 
 class DishForm(ModelForm):
     class Meta:
@@ -15,12 +16,14 @@ class DishForm(ModelForm):
     time = TimeField('Время', [validators.DataRequired()])
     button_save = SubmitField('Сохранить')
 
+
 class IngredForm(ModelForm):
     class Meta:
         model = Ingredient
 
     ingred_name = StringField('Название ингредиента', [validators.DataRequired()])
     button_save = SubmitField('Сохранить')
+
 
 class UnitForm(ModelForm):
     class Meta:
@@ -29,18 +32,56 @@ class UnitForm(ModelForm):
     unit_name = StringField('Единица измерения', [validators.DataRequired()])
     button_save = SubmitField('Сохранить')
 
+
 class DishIngredForm(ModelForm):
     class Meta:
         model = Dish_Ingredient
 
-    dish_name = QuerySelectField('Блюдо',
-                             query_factory=lambda: mydb.session.query(Dish).all(),
-                             get_pk=lambda g: g.id_dish,
-                             get_label=lambda g: "%s" % (g.dish_name))
-
+    dish = QuerySelectField('Блюдо',
+                            query_factory=lambda: mydb.session.query(Dish).all(),
+                            get_pk=lambda g: g.id_dish,
+                            get_label=lambda g: "%s" % (g.dish_name))
     # id_dish = IntegerField('Айди блюда', [validators.DataRequired()])
-    id_ingred = IntegerField('Айди ингредиента', [validators.DataRequired()])
-    id_unit = IntegerField('Айди единицы', [validators.DataRequired()])
+    ingred = QuerySelectField('Ингредиент',
+                              query_factory=lambda: mydb.session.query(Ingredient).all(),
+                              get_pk=lambda g: g.id_ingred,
+                              get_label=lambda g: "%s" % (g.ingred_name))
+    unit = QuerySelectField('Единица измерения',
+                            query_factory=lambda: mydb.session.query(Unit).all(),
+                            get_pk=lambda g: g.id_unit,
+                            get_label=lambda g: "%s" % (g.unit_name))
     sum = IntegerField('Количество', [validators.DataRequired()])
     button_save = SubmitField('Сохранить')
 
+
+class FormSearch(Form):
+    dish_name = StringField('Название блюда')
+    sum = IntegerField("Количество ингредиентов")
+    ingred_names = StringField('Список ингредиентов (через пробел)')
+    # ingred = QuerySelectField('Ингредиент',
+    #                           query_factory=lambda: mydb.session.query(Ingredient).all(),
+    #                           get_pk=lambda g: g.id_ingred,
+    #                           get_label=lambda g: "%s" % (g.ingred_name))
+    button_search3 = SubmitField('Поиск по ингредиентам')
+    button_search1 = SubmitField('Поиск по названию')
+    button_search2 = SubmitField('Установить')
+
+    def set_ingred(self, k: int):
+        for i in range(k):
+            self.ingred = []
+            ing_tmp = QuerySelectField('Ингредиент',
+                              query_factory=lambda: mydb.session.query(Ingredient).all(),
+                              get_pk=lambda g: g.id_ingred,
+                              get_label=lambda g: "%s" % (g.ingred_name))
+            self.ingred.append(ing_tmp)
+
+
+
+# class SearchDishForm(Form):
+#     class Meta:
+#         model = Ingredient
+#     ingred = QuerySelectField('Ингредиент',
+#                               query_factory=lambda: mydb.session.query(Ingredient).all(),
+#                               get_pk=lambda g: g.id_ingred,
+#                               get_label=lambda g: "%s" % (g.ingred_name))
+#     button_search1 = SubmitField('Поиск')
--- Кулинарная книга/DishBook/main.py
+++ Кулинарная книга/DishBook/main.py
@@ -1,31 +1,151 @@
 from flask import render_template, request, redirect, url_for
 from models import Dish, Ingredient, Unit, Dish_Ingredient
 from app_config import app, mydb
-from forms import DishForm, IngredForm, UnitForm, DishIngredForm
+from forms import DishForm, IngredForm, UnitForm, DishIngredForm, FormSearch
+
 
 @app.route('/')
 def menu():
     return render_template('./menu.html')
 
+
 @app.route('/dish')
 def dish():
     dishes = mydb.session.query(Dish).all()
-    return render_template('dish.html', dishes=dishes)
+    form = FormSearch(request.args)
+    if form.button_search1.data:
+        q = mydb.session.query(Dish)
+        if form.dish_name.data != '':
+            q = q.filter(Dish.dish_name == form.dish_name.data)
+        dishes = q.all()
+
+    return render_template('dish.html', dishes=dishes, form=form)
+
 
 @app.route('/ingredient')
 def ingredient():
     ingreds = mydb.session.query(Ingredient).all()
     return render_template('ingredient.html', ingred=ingreds)
 
+
 @app.route('/dish_ingred')
 def dish_ingred():
     dish_ingred = mydb.session.query(Dish_Ingredient).all()
     return render_template('dish_ingred.html', dish_ingred=dish_ingred)
 
+
 @app.route('/unit')
 def unit():
     units = mydb.session.query(Unit).all()
     return render_template('unit.html', units=units)
+
+# @app.route('/search_dish')
+# def search_dish():
+#     form = SearchDishForm(request.args)
+#     dishes = mydb.session.query(Dish_Ingredient).all()
+#     if form.button_search1.data:
+#         print(form.ingred.data)
+#             # q = mydb.session.query(Dish_Ingredient)
+#             # if form.ingred.data != '':
+#             #     ingreds_id = [int(elem) for elem in form.ingred_names.data.split(sep=',')]
+#             #
+#             #     print(ingreds_id)
+#             #
+#             #     q = get_min_dishes(ingreds_id)
+#             # dish = q
+#             # dish_id = [elem.id_dish for elem in dish]
+#             # dishes = get_dish_ingred(dish_id)
+#         dishes_id = []
+#         new_dishes = []
+#         ingreds = {}
+#         for dish in dishes:
+#             if not dishes_id.__contains__(dish.id_dish):
+#                 new_dishes.append(dish)
+#                 dishes_id.append(dish.id_dish)
+#             if not ingreds.keys().__contains__(dish.id_dish):
+#                 ingreds[dish.id_dish] = []
+#             ingreds[dish.id_dish].append(
+#                 str(dish.ingred.ingred_name) + ' - ' + str(dish.sum) + str(dish.unit.unit_name))
+#
+#         return render_template('beauty_dish.html', dish=new_dishes, ingreds=ingreds, form=form)
+
+
+@app.route('/beauty_dish')
+def beauty_dish():
+    form = FormSearch(request.args)
+    dishes = mydb.session.query(Dish_Ingredient).all()
+
+    if form.button_search1.data:
+        q = mydb.session.query(Dish)
+        if form.dish_name.data != '':
+            q = q.filter(Dish.dish_name == form.dish_name.data)
+        dish = q.all()
+        dish_id = [elem.id_dish for elem in dish]
+        dishes = get_dish_ingred(dish_id)
+
+    if form.button_search2.data:
+        if form.sum.data is not None:
+            s = form.sum.data
+            # sum = [i for i in range(s)]
+            form.set_ingred(int(s))
+            print(form.ingred)
+            return render_template('beauty_dish.html', form=form)
+
+    if form.button_search3.data:
+        print(form.ingred.data)
+        # q = mydb.session.query(Dish_Ingredient)
+        # if form.ingred_names.data != '':
+        #     ingreds_id = [int(elem) for elem in form.ingred_names.data.split(sep=',')]
+        #
+        #     print(ingreds_id)
+        #
+        #     q = get_min_dishes(ingreds_id)
+        # dish = q
+        # dish_id = [elem.id_dish for elem in dish]
+        # dishes = get_dish_ingred(dish_id)
+    dishes_id = []
+    new_dishes = []
+    ingreds = {}
+    for dish in dishes:
+        if not dishes_id.__contains__(dish.id_dish):
+            new_dishes.append(dish)
+            dishes_id.append(dish.id_dish)
+        if not ingreds.keys().__contains__(dish.id_dish):
+            ingreds[dish.id_dish] = []
+        ingreds[dish.id_dish].append(str(dish.ingred.ingred_name) + ' - ' + str(dish.sum) + str(dish.unit.unit_name))
+
+    return render_template('beauty_dish.html', dish=new_dishes, ingreds=ingreds, form=form)
+
+def get_min_dishes(max_ingreds_id : list):
+    all_dishes = mydb.session.query(Dish_Ingredient)
+    dishes = mydb.session.query(Dish)
+    ingreds_dict = {}
+    for dish in dishes:
+        if not ingreds_dict.keys().__contains__(dish.id_dish):
+            ingreds_dict[dish.id_dish] = []
+    print(all_dishes)
+    for dish in all_dishes:
+        ingreds_dict[dish.id_dish].append(dish.id_ingred)
+    min_dishes = []
+    for dish_id in ingreds_dict.keys():
+        if contains(ingreds_dict[dish_id], max_ingreds_id):
+            min_dishes.append(dish_id)
+    print(min_dishes)
+    return get_dish_ingred(min_dishes)
+
+def get_dish_ingred(dishes_id: list):
+    new_dish_ingreds = []
+    dish_ingreds = mydb.session.query(Dish_Ingredient).all()
+    for elem in dish_ingreds:
+        if dishes_id.__contains__(elem.id_dish):
+            new_dish_ingreds.append(elem)
+    return new_dish_ingreds
+
+def contains(minlist: list, list: list):
+    for elem in minlist:
+        if not elem in list:
+            return False
+    return True
 
 @app.route('/change_dish/<id>/<do>', methods=['GET', 'POST'])
 def change_dish(id, do):
@@ -49,8 +169,8 @@ def change_dish(id, do):
         if s.id_dish != id:
             return redirect(url_for('dish', id=s.id_dish))
 
-
     return render_template('change_dish.html', form=form)
+
 
 @app.route('/change_ingred/<id>/<do>', methods=['GET', 'POST'])
 def change_ingred(id, do):
@@ -74,8 +194,8 @@ def change_ingred(id, do):
         if s.id_ingred != id:
             return redirect(url_for('ingredient', id=s.id_ingred))
 
-
     return render_template('change_ingred.html', form=form)
+
 
 @app.route('/change_unit/<id>/<do>', methods=['GET', 'POST'])
 def change_unit(id, do):
@@ -99,8 +219,8 @@ def change_unit(id, do):
         if s.id_unit != id:
             return redirect(url_for('unit', id=s.id_unit))
 
-
     return render_template('change_unit.html', form=form)
+
 
 @app.route('/change_dish_ingred/<id>/<do>', methods=['GET', 'POST'])
 def change_dish_ingred(id, do):
@@ -124,10 +244,17 @@ def change_dish_ingred(id, do):
         if s.id_dish_ingred != id:
             return redirect(url_for('dish_ingred', id=s.id_dish_ingred))
 
-
     return render_template('change_dish_ingred.html', form=form)
+
+@app.route('/search_dish')
+def search_dish():
+    form = FormSearch(request.form)
+    if form.button_search3.data:
+        return beauty_dish()
+    return redirect(url_for('search_dish', form=form))
 
 
 if __name__ == '__main__':
+    metadata = mydb.metadata
+    metadata.create_all(mydb.engine)
     app.run()
-
