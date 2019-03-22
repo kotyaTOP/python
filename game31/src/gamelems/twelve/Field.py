0d03c02a90ad513a9dd0046d2df56ff4258f8048
kriskamumu <malenkauuuu@yandex.ru>
2019-03-22 11:40:21 +0300

    пп

--- game31/src/gamelems/twelve/Field.py
+++ game31/src/gamelems/twelve/Field.py
@@ -16,73 +16,74 @@ class Field:
         self.add_random_square()
         self.add_random_square()
 
-    def create_field(self):
-        for r in range(self.row_count):
-            one_row = []
-            for c in range(self.col_count):
-                one_row.append(None)
-            self.matrix.append(one_row)
+    # def create_field(self):
+    #     for r in range(self.row_count):
+    #         one_row = []
+    #         for c in range(self.col_count):
+    #             one_row.append(None)
+    #         self.matrix.append(one_row)
 
-    def none_count(self):
-        count = 0
-        for row in self.matrix:
-            for elem in row:
-                if elem is None:
-                    count += 1
-        return count
+    # def none_count(self):
+    #     count = 0
+    #     for row in self.matrix:
+    #         for elem in row:
+    #             if elem is None:
+    #                 count += 1
+    #     return count
+    #
+    # def check_lose(self):
+    #     return not self.none_count()
 
-    def check_lose(self):
-        return not self.none_count()
-
-    def add_random_square(self):
-        new_place = randint(1, self.none_count())
-        count = 0
-        for y, row in enumerate(self.matrix):
-            for x, square in enumerate(row):
-                if square is None:
-                    count += 1
-                    if count == new_place:
-                        new_square = Square(y, x, self.random())
-                        self.matrix[y][x] = new_square
-                        break
-
-    @staticmethod
-    def random():
-        percent = randint(1, 15)
-        if percent <= 10:
-            return 1
-        if 10 < percent <= 13:
-            return 2
-        if percent >= 14:
-            return 3
-
-    def add_square(self, square: Square, new_row, new_col):
-        square.col = new_col
-        square.row = new_row
-        self.matrix[new_row][new_col] = Square(new_row, new_col, square.val)
-
-    def delete_square(self, square: Square):
-        self.matrix[square.row][square.col] = None
+    # def add_random_square(self):
+    #     new_place = randint(1, self.none_count())
+    #     count = 0
+    #     for y, row in enumerate(self.matrix):
+    #         for x, square in enumerate(row):
+    #             if square is None:
+    #                 count += 1
+    #                 if count == new_place:
+    #                     new_square = Square(y, x, self.random())
+    #                     self.matrix[y][x] = new_square
+    #                     break
+    # #
+    # @staticmethod
+    # def random():
+    #     percent = randint(1, 15)
+    #     if percent <= 10:
+    #         return 1
+    #     if 10 < percent <= 13:
+    #         return 2
+    #     if percent >= 14:
+    #         return 3
+    #
+    # def add_square(self, square: Square, new_row, new_col):
+    #     square.col = new_col
+    #     square.row = new_row
+    #     self.matrix[new_row][new_col] = Square(new_row, new_col, square.val)
+    #
+    # def delete_square(self, square: Square):
+    #     self.matrix[square.row][square.col] = None
 
     def move_square(self, square: Square, new_row, new_col):
-        if self.has_ways(square, new_row, new_col):
+!        if self.has_ways(square, new_row, new_col):
             if self.matrix[new_row][new_col] is None:
                 self.simple_move(square, new_row, new_col)
+
             else:
                 val2 = self.matrix[new_row][new_col].val
                 if val2 == square.val:
                     square2 = Square(new_row, new_col, val2)
                     self.merge_square(square, square2)
-                    self.add_square(square2, square2.row, square2.col)
+?                    self.add_square(square2, square2.row, square2.col)
 
-    def simple_move(self, square: Square, new_row, new_col):
-        tmp_square = square.__copy__()
-        self.add_square(tmp_square, new_row, new_col)
-        self.delete_square(square)
-
-    def merge_square(self, square1: Square, square2: Square):
-        self.delete_square(square1)
-        square2.val += 1
+    # def simple_move(self, square: Square, new_row, new_col):
+    #     tmp_square = square.__copy__()
+    #     self.add_square(tmp_square, new_row, new_col)
+    #     self.delete_square(square)
+    #
+    # def merge_square(self, square1: Square, square2: Square):
+    #     self.delete_square(square1)
+    #     square2.val += 1
 
     def get_nei_list(self, row, col):
         nei_list = []
@@ -107,7 +108,7 @@ class Field:
             neighbour_list = self.get_nei_list(node[0], node[1])
             if len(neighbour_list) > 0:
                 for neighbour in neighbour_list:
-                    if g.nodes().__contains__((neighbour[0], neighbour[1])):
+                      if g.nodes().__contains__((neighbour[0], neighbour[1])):
                         g.add_edge(node, (neighbour[0], neighbour[1]))
         return g.edges()
 
